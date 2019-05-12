13a5ba40803a60b695cbd30157f1b29dc91476fb
DaniBlaney <daniblaney@gmail.com>
2019-05-11 23:02:01 -0400

    buildword func

--- word.js
+++ word.js
@@ -1,1 +1,10 @@
 var Letter = require("./letters.js");
+
+var Word = function (word) {
+
+  this.buildWord = function (word) {
+      var lettersBank = [];
+      for (var i = 0; i < word.length; i++) {
+          var currentLetter = new Letter(word[i]);
+          lettersBank.push(currentLetter);
+      }
