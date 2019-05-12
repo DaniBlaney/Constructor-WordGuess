5d0edfb62da48e1a1c2553408146fb0843c8bf06
DaniBlaney <daniblaney@gmail.com>
2019-05-11 23:16:14 -0400

    created array of words

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var inquirer = require("inquirer");
 
 var Word = require("./word.js");
+
+var guesses = 10;
+var points = 0;
+
+var wordsToGuess = ["Thanos", "Spider Man", "Iron Man", "Captain America", "Thor", "Black Widow", "Hulk", "Ant-Man", "War Machine", "Captain Marvel", "Deadpool", "Nick Furry", "Star-Lord", "Gamora", "Nebula", "Ghost Rider", "Loki", "Jean Grey", "Juggernaut", "Rogue", "Wolverine", "Rocket Raccoon", "Professor X", "Thing", "Mystique"];
+var randomWord;
+var chosenWord;
+
+function startGame() {
