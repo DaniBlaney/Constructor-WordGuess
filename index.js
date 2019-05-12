561098063883f28d701a9a985558be867ffa04c4
DaniBlaney <daniblaney@gmail.com>
2019-05-11 23:20:06 -0400

    random word

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ function startGame() {
   console.log("It's time to guess Marvel characters!");
 }
 
+function chooseRandomWord() {
+
+  randomWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)]
+
+  chosenWord = new Word(randomWord);
+}
+function guessWord() {
+
+  if (guesses > 0 && points < 5) {
+
+      console.log(chosenWord.display());
+
+      inquirer.prompt([
+        {
+            name: "txt",
+            message: "Guess a letter!",
+            validate: function (str) {
+                if (str.length != 1) return false;
+                var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
+                return regEx.test(str);
+            }
+
+        }]).then(function (guessedLetter) {
+
+        var guess = guessedLetter.txt;
+
+        chosenWord.checkGuess(guess);
