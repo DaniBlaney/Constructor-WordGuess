287ba2bdd4df76240e3e2d67013cecdbb244dfae
DaniBlaney <daniblaney@gmail.com>
2019-05-16 19:53:31 -0400

    fixed double inquirer

--- index.js
+++ index.js
@@ -11,61 +11,57 @@ var chosenWord;
 
 function startGame() {
   console.log("It's time to guess Marvel characters!");
+  chooseRandomWord();
+  guessWord();
 }
 
 function chooseRandomWord() {
-
   randomWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)]
-
   chosenWord = new Word(randomWord);
 }
 
 function guessWord() {
-
   if (guesses > 0 && points < 5) {
-
-      console.log(chosenWord.display());
-
-      inquirer.prompt([
-        {
-            name: "txt",
-            message: "Guess a letter!",
-            validate: function (str) {
-                if (str.length != 1) return false;
-                var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
-                return regEx.test(str);
-            }
-
-        }]).then(function (guessedLetter) {
-
-        var guess = guessedLetter.txt;
-
-        chosenWord.checkGuess(guess);
-        if (randomWord.toLowerCase().indexOf(guess.toLowerCase()) === -1) {
-          guesses--;
-          console.log("INCORRECT! " + guesses + " guesses remaining")
-        }else {
-          if (points < 5) {
-          console.log("CORRECT!")
+    console.log(chosenWord.display());
+    inquirer.prompt([
+      {
+          name: "txt",
+          message: "Guess a letter!",
+          validate: function (str) {
+              if (str.length != 1) return false;
+              var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
+              return regEx.test(str);
           }
-        }
-        if (randomWord === chosenWord.display()) {
-          console.log(chosenWord.display());
-          guesses = 10;
-          points++;
-
-          if (points < 5) {
-              // console.log("CORRECT! Next character!");
-              winGame();
-            }
 
       }
-
-        if (guesses === 0) {
-            loseGame();
-        }
-
+    ]).then(function (guessedLetter) {
+      var guess = guessedLetter.txt;
+      chosenWord.checkGuess(guess);
+      if (randomWord.toLowerCase().indexOf(guess.toLowerCase()) === -1) {
+        guesses--;
+        console.log("INCORRECT! " + guesses + " guesses remaining")
         guessWord();
+      } else {
+        if (points < 5) {
+        console.log("CORRECT!")
+        guessWord();
+        }
+      }
+      if (randomWord === chosenWord.display()) {
+        console.log(chosenWord.display());
+        guesses = 10;
+        points++;
+        if (points < 5) {
+          // console.log("CORRECT! Next character!");
+
+          winGame();
+        }
+      }
+
+      if (guesses === 0) {
+          loseGame();
+      }
+
 
     });
   }
@@ -105,20 +101,20 @@ function winGame() {
         message: "Play again?",
         default: true
     }
-]).then(function (inquirerResponse) {
-  if (inquirerResponse.confirm) {
-      guesses = 10;
-      points = 0;
-      chooseRandomWord();
-      guessWord();
-  }
-  else {
-      console.log("See ya next time!")
-      process.exit();
-  }
-})
-
+  ]).then(function (inquirerResponse) {
+    if (inquirerResponse.confirm) {
+        guesses = 10;
+        points = 0;
+        // chooseRandomWord();
+        // guessWord();
+        startGame();
+    }
+    else {
+        console.log("See ya next time!")
+        process.exit();
+    }
+  })
 }
 startGame();
-chooseRandomWord();
-guessWord();
+// chooseRandomWord();
+// guessWord();
